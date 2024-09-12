d6f49107610abcabd204173c500389b141ebed2b
Abhijeet <abhijeetsuryawanshi128@gmail.com>
2024-09-12 16:47:45 +0530

    challenge 1

--- count_the_number_of_consistent_string.py
+++ count_the_number_of_consistent_string.py
@@ -1,8 +1,7 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         allowed_set = set(allowed)
-        consistent_count = 0
-        
+        consistent_count = 0  
         for w in words:
             if all(char in allowed_set for char in w):
                 consistent_count += 1
